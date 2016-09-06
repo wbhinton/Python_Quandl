96378c7bb61526d062b02e98a07264f822849111
Weston Hinton <wbhinton@gmail.com>
2016-09-06 13:57:40 -0500

    Quandl Initialization Script (API Key)

---
+++ initial.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import quandl
+import seaborn 
+import numpy
+import pandas
+"""This sets the Quandl API Key"""
+
+quandl.ApiConfig.api_key="-RHX3yVqCy9ZPgpmf2h3"
