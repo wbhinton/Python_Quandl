4d3fcf82a4bbed69eeefd5ea28f1aa8956aa0750
wbhinton <wbhinton@gmail.com>
2016-09-09 12:01:18 -0500

    Cleaned up some code

--- fund_analy_single.py
+++ fund_analy_single.py
@@ -22,18 +22,23 @@ import ystockquote
 import datetime
 
 """This sets the Quandl API Key"""
+
 ql.ApiConfig.api_key="YOUR API KEY"
+
 """Set stock symbol"""
 tic=input('Please enter stock symbol:') 
+
 """Set current date and 1yr prior"""
 now=datetime.date.today()
 year=datetime.timedelta(365)
 yearago=now-year
 lastyear=yearago.strftime("%Y-%m-%d")
+
 """Collect the Data"""
 CurrentPrice=float(ystockquote.get_price(tic))
 LastYearQuote=ystockquote.get_historical_prices(tic,lastyear,lastyear)
 LastYearPrice=float(LastYearQuote.get(lastyear).get('Close'))
+
 """YearAgoPrice="""
 Receivables=pd.DataFrame(ql.get("SF0/%s_RECEIVABLES_MRY"%tic))
 Inventory=pd.DataFrame(ql.get("SF0/%s_INVENTORY_MRY"%tic))
@@ -56,9 +61,9 @@ NetIncome=pd.DataFrame(ql.get("SF0/%s_NETINC_MRY"%tic))
 CurrentRatio=pd.DataFrame(ql.get("SF0/%s_CURRENTRATIO_MRY"%tic))
 DebtToEquity=pd.DataFrame(ql.get("SF0/%s_DE_MRY"%tic))
 
-
 """Calculations-- BuyRating is how we are rating the stock on a scale of 1-8"""
 BuyRating=0
+
 """Profit Margin--- CNPM is Current Net Profit Margin"""
 GrossProfitMargin=GrossProfit/Revenue+100
 NetProfitMargin=NetIncome/Revenue*100
@@ -66,12 +71,10 @@ CNPM=NetProfitMargin.iat[0,0]
 if CNPM<15:
     BuyRating=BuyRating-8
  
-
 """If CurrentEPS is greater than zero, then the acceptable
 price for stock is given by:"""   
 CurrentEPS=EPS.iat[0,0]
 PreviousEPS=EPS.iat[1,0]
-
 AcceptablePrice=0
 if CurrentEPS>0 :
     AcceptablePrice=(CurrentEPS/PreviousEPS)*CurrentPrice
@@ -84,7 +87,6 @@ PreviousPE=LastYearPrice/PreviousEPS
 if CurrentEPS>0:
     BuyRating=BuyRating+int((CurrentPE/PreviousPE)*100)/100
 
-
 """Quick Ratio Calc"""
 TCA=TotalCurrentAssets.iat[0,0]
 Inv=Inventory.iat[0,0]
@@ -92,6 +94,7 @@ TCL=TotalCurrentLiabilities.iat[0,0]
 QuickRatio=(TCA-Inv)/TCL
 if QuickRatio>.7:
     BuyRating=BuyRating+1
+
 """Current Ratio Test"""
 CurRatio=CurrentRatio.iat[0,0]
 if CurRatio>1.5:
@@ -122,27 +125,31 @@ DPR=(DeclaredDividend.iat[0,0]/CurrentEPS)*100
 if DPR>40:
     BuyRating=BuyRating+1
 
-"""Change in Sales"""
-ChangeInSales=(Revenue.iat[0,0]-Revenue.iat[1,0])/Revenue.iat[1,0]    
-
 """Company Performance"""
+ChangeInSales=(Revenue.iat[0,0]-Revenue.iat[1,0])/Revenue.iat[1,0]    
 CoPerform=(ChangeInSales/((Inventory.iat[0,0]+Receivables.iat[0,0])-(Inventory.iat[1,0]+Receivables.iat[1,0])))/(Inventory.iat[1,0]+Receivables.iat[1,0])
 if CoPerform>ChangeInSales:
     Performance="Underperform"
 Performance="OK"
 
+"""Print and Write Out Data"""
 print("./n")    
 print("Buy Rating: "+str(BuyRating)) 
 if BuyRating<5:
     print("Not Interested")
+
 elif 5<=BuyRating<6:
     print("Watch for Future Buy")
+
 elif 6<=BuyRating<7:
     print("Buy with Available Cash")
+
 elif 7<=BuyRating<8:
     print("Consider Buying on Margin")    
+
 elif BuyRating>=8:
     print("BUY BUY BUY")
+
 print("EBITDA: "+str(EBITDA.iat[0,0]))
 print("Current PE: "+str(CurrentPE) )
 print("Last Year's PE: "+str(PreviousPE))
